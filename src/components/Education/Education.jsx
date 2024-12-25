1a5ca9cbbc3d055f1d714e00f857fdd272539418
lalit <lalit.pratap.naidu@gmail.com>
2024-12-25 12:22:41 -0500

    education page

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,8 +1,19 @@
+import "./Education.css";
+
 export function Education() {
   return (
     <>
-      <h1>Education</h1>
-      <div>Education</div>
+      <h2>EDUCATION</h2>
+      <div className="education">
+        <span className="title-duration">
+          <h3 className="title">Bachelor's Degree</h3>
+          <h5 className="duration">August 2014 - July 2019</h5>
+        </span>
+        <h5 className="school">
+          NITTE MEENAKSHI INSTITUTE OF TECHNOLOGY | BENGALURU,KARNATAKA,INDIA
+        </h5>
+        <h5 className="subject">Computer Science and Engineering</h5>
+      </div>
     </>
   );
 }
