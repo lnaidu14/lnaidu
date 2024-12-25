ab3fd040b1cf9548f33f18ccb47001853d10297a
lalit <lalit.pratap.naidu@gmail.com>
2024-12-24 20:50:39 -0500

    added pixel theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bgImg"></div>
+      <div className="main-container nes-pointer">
+        <div className="content">
+          <div class="nes-container with-title is-centered">
+            <p class="title">Greetings!</p>
+            <p>
+              My name is Lalit Naidu. I am a software developer with experience
+              and expertise in frontend, backend, QA and cloud development. Look
+              below to checkout below, or if you want to get in touch with me,
+              take a look
+            </p>
+          </div>
+          <div className="social-media-links">
+            <div className="social-media-link">
+              <i class="nes-icon linkedin is-large"></i>
+              <i class="nes-icon github is-large"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
