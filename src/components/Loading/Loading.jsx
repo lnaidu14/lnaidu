c1de9f19cb1340eee0300dcd5bdf17ed8ee5514b
lnaidu14 <lalit.pratap.naidu@gmail.com>
2024-12-26 01:14:38 -0500

    added progress bar functionality

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,28 @@
 import "./Loading.css";
+import { useState, useEffect } from "react";
 export function Loading({ setDisplayPage }) {
+  const [progressBarValue, setProgressBarValue] = useState(0);
+  useEffect(() => {
+    if (progressBarValue === 100) {
+      setDisplayPage("main");
+    }
+    const id = setInterval(
+      () =>
+        setProgressBarValue((oldProgressBarValue) => oldProgressBarValue + 10),
+      100
+    );
+
+    return () => {
+      clearInterval(id);
+    };
+  });
   return (
     <>
       <div className="loading">
         <p>Loading...</p>
         <progress
           className="nes-progress is-success progress-bar"
-          value="50"
+          value={progressBarValue}
           max="100"
         ></progress>
       </div>
