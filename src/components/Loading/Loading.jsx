5abfbef2e3709fbb577b457a605e68684cf05cc2
lalit <lalit.pratap.naidu@gmail.com>
2024-12-26 17:00:21 -0500

    added loading screen and fade out effect

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,15 +2,23 @@ import "./Loading.css";
 import { useState, useEffect } from "react";
 export function Loading({ setDisplayPage }) {
   const [progressBarValue, setProgressBarValue] = useState(0);
+  const [opacityValue, setOpacityValue] = useState(0);
   useEffect(() => {
     if (progressBarValue === 100) {
-      setDisplayPage("main");
+      if (opacityValue >= 1) {
+        setDisplayPage("main");
+      }
+      const id = setInterval(
+        () => setOpacityValue((oldOpacityValue) => oldOpacityValue + 0.033),
+        100
+      );
+      return () => {
+        clearInterval(id);
+      };
     }
-    const id = setInterval(
-      () =>
-        setProgressBarValue((oldProgressBarValue) => oldProgressBarValue + 10),
-      100
-    );
+    const id = setInterval(() => {
+      setProgressBarValue((oldProgressBarValue) => oldProgressBarValue + 10);
+    }, 100);
 
     return () => {
       clearInterval(id);
@@ -19,6 +27,7 @@ export function Loading({ setDisplayPage }) {
   return (
     <>
       <div className="loading">
+        <div className="fade-in" style={{ opacity: opacityValue }}></div>
         <p>Loading...</p>
         <progress
           className="nes-progress is-success progress-bar"
