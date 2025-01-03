f50230e14ecacd67299b0146bdc37d13564c5ce0
lalit <lalit.pratap.naidu@gmail.com>
2025-01-02 20:36:40 -0500

    changed vite default base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/technodevelopcorp.github.io/",
+  // base: "/lpnaidu.github.io/",
   plugins: [react()],
 })
