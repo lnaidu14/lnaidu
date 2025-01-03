7e8d72542ab5db514f9010db2f072ce584dfb807
lalit <lalit.pratap.naidu@gmail.com>
2025-01-02 20:25:23 -0500

    added logic to check if user has visited the website

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,20 @@
 import "./Landing.css";
+import { useEffect, useState } from "react";
 export function Landing({ setDisplayPage }) {
+  useEffect(() => {
+    const visited = JSON.parse(localStorage.getItem("visited"));
+    if (visited) {
+      setDisplayPage("main");
+    }
+  }, []);
   return (
     <>
       <div
         className="landing-container"
-        onClick={() => setDisplayPage("loading")}
+        onClick={() => {
+          localStorage.setItem("visited", JSON.stringify(true));
+          setDisplayPage("loading");
+        }}
       >
         <h3 className="press-start">CLICK ANYWHERE TO START</h3>
         <div className="bg-img"></div>
