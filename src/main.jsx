475ae0cd2e56aa56347d49c0d911b0e46c90b005
lalit <lalit.pratap.naidu@gmail.com>
2025-01-04 13:38:55 -0500

    added font to assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import pixelFont from "./assets/VCR_OSD_MONO_1.001.ttf";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <style jsx>
+      {`
+        @font-face {
+          font-family: "VCR";
+          src: local("VC"), url("${pixelFont}") format("truetype");
+        }
+
+        body {
+          font-family: "VCR", sans-serif;
+          font-size: 1.5em;
+        }
+      `}
+    </style>
+  </StrictMode>
+);
