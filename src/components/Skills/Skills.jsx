df20389d1ad3ce349e98d10c5687afe66fc5ce63
lalit <lalit.pratap.naidu@gmail.com>
2025-01-04 13:32:19 -0500

    changed fonts and minor fixes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,71 +6,71 @@ export function Skills() {
       <h2>SKILLS</h2>
       <div className="skills nes-container with-title">
         <p className="title">Primary Programming Languages</p>
-        <span className="skills">
-          <div>JavaScript · TypeScript · SQL</div>
+        <span className="skill">
+          <div>JavaScript | TypeScript | SQL</div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Secondary Programming Languages</p>
-        <span className="skills">
-          <div>Python · Golang</div>
+        <span className="skill">
+          <div>Python | Golang</div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Cloud Providers and Services</p>
-        <span className="skills">
+        <span className="skill">
           <div>
             Amazon Web Services (Lambda,ECS,ECR,API
-            Gateway,SNS,SQS,Cognito,CloudWatch,IoT Core) · Microsoft Azure
+            Gateway,SNS,SQS,Cognito,CloudWatch,IoT Core) | Microsoft Azure
             (Azure AD)
           </div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">CICD</p>
-        <span className="skills">
-          <div>GitHub Actions · GitLab CICD · Terraform</div>
+        <span className="skill">
+          <div>GitHub Actions | GitLab CICD | Terraform</div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Frameworks and Libraries</p>
-        <span className="skills">
+        <span className="skill">
           <div>
-            Node.js · Next.js · React.js · Fiber · Express.js · Jest · Prime
-            React · Bootstrap · React Testing Library · Playwright · Cypress ·
-            Storybook · React Query · Jotai · Knex
+            Node.js | Next.js | React.js | Fiber | Express.js | Jest | Prime
+            React | Bootstrap | React Testing Library | Playwright | Cypress |
+            Storybook | React Query | Jotai | Knex
           </div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Databases</p>
-        <span className="skills">
-          <div>PostgreSQL · MongoDB · SQLite</div>
+        <span className="skill">
+          <div>PostgreSQL | MongoDB | SQLite</div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Tools</p>
-        <span className="skills">
+        <span className="skill">
           <div>
-            Docker · Git · Visual Studio Code · POSTMAN · Package Managers (npm,
+            Docker | Git | Visual Studio Code | POSTMAN | Package Managers (npm,
             pip, go.mod)
           </div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Concepts</p>
-        <span className="skills">
+        <span className="skill">
           <div>
-            RESTful APIs · Serverless · Infrastructure as Code (IaC) · Agile
-            Methodology (Scrum, Kanban) · Testing and Quality Assurance
+            RESTful APIs | Serverless | Infrastructure as Code (IaC) | Agile
+            Methodology (Scrum, Kanban) | Testing and Quality Assurance
           </div>
         </span>
       </div>
       <div className="skills nes-container with-title">
         <p className="title">Soft Skills</p>
-        <span className="skills">
+        <span className="skill">
           <div>
-            Teamwork · Collaboration · Written and Verbal Communication ·
+            Teamwork | Collaboration | Written and Verbal Communication |
             Conflict Resolution
           </div>
         </span>
